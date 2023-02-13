5bfc5ea9fe2ed919fe89f34c5ca594152b1d4d85
Ariam Yonas <ariam.yonas@hotmail.de>
2023-02-13 15:28:38 -0800

    CORS changes

--- app/__init__.py
+++ app/__init__.py
@@ -50,6 +50,6 @@ def create_app():
     from .routes import location_bp
     app.register_blueprint(location_bp)
 
-    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+    CORS(app, origins=["https://sblackownedproxy.herokuapp.com"])
 
     return app
